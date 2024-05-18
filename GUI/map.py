8355dc51b153338212172fc42a88af5cb399be60
drubick <valentin@DESKTOP-QGHJIOM>
2024-05-18 17:47:23 +0200

    Tab 3 added, it can import a map image and draw a grid above it, if the grid size = 0 it will draw no aditiona grid

--- GUI/map.py
+++ GUI/map.py
@@ -1,4 +1,6 @@
-from tkinter import ttk 
+from tkinter import ttk, Canvas, filedialog, PhotoImage
+from PIL import Image, ImageTk
+
 
 #This class will create a blank grid map or blank map,
 #the user can import a map image from a file.
@@ -8,5 +10,84 @@ class Map:
     def __init__(self, root):
         self._frame = ttk.Frame(root)
         self._frame.pack(expand=True, fill='both')
-
+        self._map_image_path = None
+        self._top_margin = 10
+        #default square size
+        self._scale = 50
         
+        #creates the widgets to select different options for the map
+        self.widgets_option()       
+
+        #Create a canvas to draw the grid
+        self._canvas = Canvas(self._frame)
+        self._canvas.pack(expand=True, fill='both')
+
+        #bind the draw_grid method to the frame resize eve
+        self._frame.bind('<Configure>', lambda event: self.draw_grid())
+        self._canvas.bind("<Configure>", self.update_grid)
+
+
+#This method will draw a grid on the canvas
+    def draw_grid(self):
+        #get the size of the frame
+        width = self._frame.winfo_width()
+        height = self._frame.winfo_height()
+
+        #get the size of the square grid
+        square_size = int(self._scale)
+
+        #calculate the number of squares in the x and y axis
+        x_squares = width // square_size
+        y_squares = (height - self._top_margin) // square_size
+
+        #draw the grid
+        for x in range(x_squares):
+            for y in range(y_squares):
+                x1= x * square_size
+                y1= y * square_size + self._top_margin
+                x2= x1 + square_size
+                y2= y1 + square_size
+                self._canvas.create_rectangle(x1, y1, x2, y2)
+
+    def update_grid(self, scale):
+        self._scale = int(self._scale_combox.get())
+        self._canvas.delete('all')
+        self.draw_grid()
+        self.draw_map()
+
+    def import_map(self):
+        filename = filedialog.askopenfilename(filetypes=[('Image files', '*.png *.jpg *.jpeg *.gif *.bmp')])
+        if filename:
+            self._map_image_path = filename
+            self.draw_map()
+    
+    def draw_map(self):
+        if self._map_image_path is None:
+            return 
+        canvas_width = self._canvas.winfo_width()
+        canvas_height = self._canvas.winfo_height()
+        pillow_image = Image.open(self._map_image_path)
+        pillow_image = pillow_image.resize((canvas_width, canvas_height))
+        self._map_image = ImageTk.PhotoImage(pillow_image)
+        self._canvas.delete("map")
+        self._canvas.create_image(0, 0, image=self._map_image, anchor='nw')
+        self.draw_grid()  # Call draw_grid after create_image
+
+#This method will create the widgets to select the options for the map
+    def widgets_option(self):
+        #selects the size of the square grid
+        self._scale_label = ttk.Label(self._frame, text="Select the square size of the grid:")
+        self._scale_label.pack(anchor='w')
+        self._scale_values  = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        self._scale_combox = ttk.Combobox(self._frame, values=self._scale_values)
+        self._scale_combox.set(self._scale)
+        self._scale_combox.bind('<<ComboboxSelected>>', self.update_grid)
+        
+        self._scale_combox.pack(anchor='w')
+
+        #imports a map image
+        self._import_button = ttk.Button(self._frame, text='Import Map', command=self.import_map)
+        self._import_button.pack(anchor='n')
+
+
+
--- GUI/tab_interface.py
+++ GUI/tab_interface.py
@@ -3,33 +3,40 @@ from GUI import *
 from GUI.Creature_selection import Item_search
 from GUI.encounterMode import EncounterMode
 from database.bestiary_database	import Database
+from GUI.map import Map
 
 
 
 class Tabs_interface:
     def __init__(self, root):
+        
         self._frame = ttk.Frame(root)
         self._database = Database()
         self._frame.pack(expand=True, fill='both')
         self._notebook = ttk.Notebook(self._frame)
-        #tab1
-        self.tab1 = ttk.Frame(self._notebook)
-        self._creature_selection = Item_search(self.tab1)
-        #tab2
-        self.tab2 = ttk.Frame(self._notebook)
-       
-        self._notebook.add(self.tab1, text='Tab 1')
-        self._notebook.add(self.tab2, text='Tab 2')
         
+        #tab1 this will allow to select a list of monsters or generat a random encounter.
+        self.tab1 = ttk.Frame(self._notebook)
+        self._notebook.add(self.tab1, text='Tab 1')
+        self._creature_selection = Item_search(self.tab1)
+        #tab2 this will display all of the monster stats and allow to add them to the encounter
+        self.tab2 = ttk.Frame(self._notebook)
+        self._notebook.add(self.tab2, text='Tab 2')
         self._encounter_mode = EncounterMode(self.tab2, self._database)
+        #tab3 this will allow to create a map and add tokens to it
+        self.tab3 = ttk.Frame(self._notebook)
+        self._notebook.add(self.tab3, text='Tab 3')
+        self._map = Map(self.tab3)
+       
         self._notebook.pack(expand=True, fill='both')
-
         self._notebook.bind("<<NotebookTabChanged>>", self.on_tab_changed)
 
         
         #notebook widget
     def on_tab_changed(self, event):
         selected_tab_index = self._notebook.index(self._notebook.select())
+        if selected_tab_index == 2:
+            self._map.draw_grid()
         if selected_tab_index == 1:  # tab indices are 0-based, so tab2 is at index 1
             self.add_database_monster()
         if selected_tab_index == 0:
